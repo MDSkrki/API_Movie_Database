6fa69d7d48fe515ff526c27c4955f008ae5263f7
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-13 10:05:46 +0100

    Basic get successful

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const app = express();
 app.listen(5000, ()=>{ console.log('Server successfully initiated at port 5000')});
 
 app.use(express.json());
+
+app.get('/', (req,res) => {
+    console.log('Get request successful');
+    res.send('Get response successful');
+})
