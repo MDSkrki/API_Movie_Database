3d95909697510717698fbd2d60971b3f2dfcea02
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-14 16:38:59 +0100

    created get for all users from database

--- user/controller.js
+++ user/controller.js
@@ -1,6 +1,11 @@
 import User from "./model.js";
 
-export const getUser = async (req, res) => {
+export const getAllUsers = async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+}
+
+export const getUserById = async (req, res) => {
     const user = await User.findById(req.params.id);
     res.json(user);
 }
--- user/router.js
+++ user/router.js
@@ -2,7 +2,8 @@ import express from "express";
 import * as controller from './controller.js';
 const router = express.Router();
 
-router.get('/:id',controller.getUser); // Returns User if id is correct
+router.get('/', controller.getAllUsers);
+router.get('/:id',controller.getUserById); // Returns User if id is correct
 router.post('/', controller.createUser);
 router.delete('/:id', controller.deleteUserById);
 router.patch('/:id', controller.updateUserById);
