b2de3cc4c41f408b12a3079b920e6dd1aac73fad
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-16 09:25:21 +0100

    wrapped controller in try-catch declarations to avoid weird crashes

--- user/controller.js
+++ user/controller.js
@@ -1,28 +1,54 @@
 import User from "./model.js";
-//TODO: catch errors
+
 
 export const getAllUsers = async (req, res) => {
-    const users = await User.find({});
-    res.json(users);
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 }
 
 export const getUserById = async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.json(user);
+    try {
+        const user = await User.findById(req.params.id);
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 }
 
 export const createUser = async (req, res) => {
-    const user = await User.create(req.body);
-    res.json(user);
+    try {
+        const user = await User.create(req.body);
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 }
 
+
 export const deleteUserById = async (req, res) => {
-    const user = await User.findByIdAndRemove(req.params.id);
-    res.json(user);
+    try {
+        const user = await User.findByIdAndRemove(req.params.id);
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
 }
 
 export const updateUserById = async (req, res) => {
-    const update = req.body;
-    const previousValue = await User.findByIdAndUpdate(req.params.id, update);
-    res.json(previousValue);
+    try {
+        const update = req.body;
+        const previousValue = await User.findByIdAndUpdate(req.params.id, update);
+        res.json(previousValue);
+    } catch (error) {
+        console.log(error);
+        res.send(erorr);
+    }
 }
