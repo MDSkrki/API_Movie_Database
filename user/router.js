40533c73c27eaf745c0fc14e9372bdd6ae34f957
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-18 09:07:41 +0100

    eliminated useless endpoint and comments

--- user/router.js
+++ user/router.js
@@ -4,8 +4,6 @@ import * as middleware from './middleware.js'
 const router = express.Router();
 
 router.get('/', middleware.authenticator('admin'), controller.getAllUsers); // Should only be allowed as admin
-//router.get('/:id',controller.getUserById); // Returns User if id is correct;
-router.post('/', middleware.authenticator('admin'), controller.createUser);
 router.delete('/:id', controller.deleteUserById); //Should only be allowed as admin or to remove itself
 router.patch('/:id', controller.updateUserById); //Should only be allowed by admin or to update itself
 
@@ -14,6 +12,6 @@ router.post('/login', controller.userLogin); // this checks if user exists and g
 
 export default router;
 
-//TODO: endopoint /login will generate token if user is found
+
 //TODO: endpoint /register will create a user
 //TODO: make endpoint to createUser and store it into database, (token not necessary, but cool to have in order to avoid login post-signUp)
