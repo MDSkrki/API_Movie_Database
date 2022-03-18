a0ba32cd5daa175c0cf45f7d20b5dae3f34dc6a2
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-18 10:18:50 +0100

    added dotenv

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ import express from 'express';
 import userRouter from './user/router.js';
 import movieRouter from './movie/router.js'
 import connection from './config/db.js';
+import dotenv from 'dotenv'
 
 
 const app = express();
-
+dotenv.config();
 
 app.listen(5000, ()=>{ console.log('Server successfully initiated at port 5000')});
 connection();
