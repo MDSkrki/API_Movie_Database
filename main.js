91dd90624ea54aed9f1a11496ee9e67214b3d088
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-18 10:53:02 +0100

    attempt at connecting to heroku

--- main.js
+++ main.js
@@ -8,11 +8,15 @@ import dotenv from 'dotenv'
 const app = express();
 dotenv.config();
 
-app.set("port", process.env.PORT || 5000)
-
-app.listen(app.get("port"), ()=>{ console.log('Server successfully initiated at port 5000')});
 connection();
 
+app.set("port", process.env.PORT || 3000)
 app.use(express.json());
 app.use('/user', userRouter);
 app.use('/movie', movieRouter);
+
+
+
+app.listen(app.get("port"), ()=>{ console.log('Server successfully initiated at port 3000')});
+
+
