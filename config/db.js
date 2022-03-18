c7777781c13c6c3f23d47c1e2fba3734a597b808
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-18 10:35:40 +0100

    attempt at connecting database to heroku

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connection = async () => {
     try {
-        await mongoose.connect('mongodb+srv://admin@cluster0.2y9ji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://admingi@cluster0.2y9ji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
         console.log('Connection to mongodb successful');
     } catch (error) {
         console.log(error, 'Connection to mongodb failed');
