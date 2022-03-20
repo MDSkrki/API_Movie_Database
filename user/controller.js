b32282fdb50d15e69ee505615f8e0de22e2c2826
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-20 15:56:19 +0100

    wrapped login in try catch because it used to crash the server

--- user/controller.js
+++ user/controller.js
@@ -62,16 +62,21 @@ export const updateUserById = async (req, res) => {
 }
 
 export const userLogin = async (req, res) => {
-    const user = await User.findOne({
-        email: req.headers.email,
-    });
-    // passCorrect checks if password provided by user is the same as the hashed version stored in database
-    const passCorrect = await service.passChecker(req.headers.password, user.password);
-    if (passCorrect) {
-        const token = service.tokenGenerator(user.role, process.env.JWT_SECRET);
-        console.log(token);
-        res.send(token)
-    } else {
-        res.status(401).send('User not found');
+    try {
+        const user = await User.findOne({
+            email: req.headers.email,
+        });
+        // passCorrect checks if password provided by user is the same as the hashed version stored in database
+        const passCorrect = await service.passChecker(req.headers.password, user.password);
+        if (passCorrect) {
+            const token = service.tokenGenerator(user.role, process.env.JWT_SECRET);
+            console.log(token);
+            res.send(token)
+        } else {
+            res.status(401).send('User not found');
+        }
+    } catch (error) {
+        console.log(error, 'Login failed');
     }
+    
 }
