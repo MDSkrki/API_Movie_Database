1102afeae75fca08e2c91218597606ea8ecd0540
Mihai Daniel Somkereki <somkereki@hotmail.com>
2022-03-20 13:35:01 +0100

    Fixed uri for db connection

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connection = async () => {
     try {
-        await mongoose.connect(process.env.LOCAL_DB);
+        await mongoose.connect(process.env.DB_URI);
         console.log('Connection to mongodb successful');
     } catch (error) {
         console.log(error, 'Connection to mongodb failed');
